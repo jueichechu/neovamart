1e245afcb79dbd2e96a739eec0ad7263c4e2e976
jueichechu <richardchu0219@outlook.com>
2025-06-27 15:44:14 -0500

    updating getProducts function

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,22 @@
+import {sql} from "../config/db.js" // import the sql function
+
 // asynchronous function because it will take some time to get data from database, and we can call await in it
-export const getAllProducts = async (req, res) => {}; // first function, which gets all products
+export const getProducts = async (req, res) => {
+    try {
+        const products = await sql`
+            SELECT * FROM products
+            ORDER BY created_at DESC
+        `;
+        console.log("fetched products", products);
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+
+    }
+}; 
+
 export const createProduct = async (req, res) => {};
+
+export const getProduct = async (req, res) => {};
+export const updateProduct = async (req, res) => {};
+export const deleteProduct = async (req, res) => {};
 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,19 @@
 import expres from "express"; 
-import { createProduct, getAllProducts } from "../controllers/productController.js"; // remember to add .js at the end of the import path
+import { 
+    getProducts, 
+    getProduct, 
+    createProduct, 
+    updateProduct, 
+    deleteProduct 
+} from "../controllers/productController.js"; // remember to add .js at the end of the import path
 
 const router = expres.Router() // create a new router instance
 
-router.get("/", getAllProducts);
+router.get("/", getProducts); // get all products, which is the default route for this router
+router.get("/:id", getProduct); // get product by id, which is passed in the URL as a parameter
 router.post("/", createProduct);
+router.put("/:id", updateProduct); // update product by id
+router.delete("/:id", deleteProduct); 
+
 
 export default router; // export the router
