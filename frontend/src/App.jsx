105f8a5bb4c9d276e80e00a6c6e13d3ccc49c12d
jueichechu <richardchu0219@outlook.com>
2025-06-28 17:05:06 -0500

    finish setting up frontend homepage and productpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage"
+
+import { Routes, Route } from "react-router-dom"; 
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500"> hey </h1>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+      
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
