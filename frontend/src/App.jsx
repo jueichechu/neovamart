e52847dafa2df255222b8cd66f2f374166bfb0ec
jueichechu <richardchu0219@outlook.com>
2025-06-29 09:31:54 -0500

    implemented navbar and theme selector

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage"
 
-import { Routes, Route } from "react-router-dom"; 
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import { Routes, Route } from "react-router-dom";
+import { useThemeStore } from "./store/useThemeStore";
 
 function App() {
+  const { theme } = useThemeStore();
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
-      
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
       <Navbar />
 
       <Routes>
@@ -18,4 +20,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 function ProductPage() {
-  return (
-    <div>ProductPage</div>
-  )
+  return <div>ProductPage</div>;
 }
 
 export default ProductPage
