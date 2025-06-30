d730ce1c4ad1528a994fca1650605f815fbd4ed3
jueichechu <richardchu0219@outlook.com>
2025-06-29 21:28:07 -0500

    starting product page

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,4 +1,26 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useProductStore } from "../store/useProductStore";
+import { useEffect } from "react";
+
 function ProductPage() {
+  const {
+    currentProduct,
+    formData,
+    setFormData,
+    loading,
+    error,
+    fetchProduct,
+    updateProduct,
+    deleteProduct,
+  } = useProductStore();
+  const navigate = useNavigate(); 
+  const {id} = useParams() // to fetch product id in the url 
+
+  useEffect(() => {
+    fetchProduct(id);
+  }, [fetchProduct, id]);
+
+  console.log(currentProduct);
   return <div>ProductPage</div>;
 }
 
